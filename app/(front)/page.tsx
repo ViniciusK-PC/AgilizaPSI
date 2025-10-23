67481b8b77035d24c4ec5050a27fc5ab87c6b30f
ViniciusK-PC <viniciuskb36@gmail.com>
2025-10-23 20:35:53 -0300

    feat: Add Navabr

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,12 +1,9 @@
-import Navbar from "@/components/Frontend/Navbar";
-
-
+import Hero from "@/components/Frontend/Hero";
 
 export default function Home() {
   return (
-    <section className="text-3xl flex min-h-screen justify-center items-center">
-    
-    <h2 className="text-3xl">AgilizaPSI</h2>
+    <section className="">
+      <Hero />
     </section>
   );
 }
