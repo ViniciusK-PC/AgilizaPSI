7a9ea535528faef8371ebcd307d809e549a55c43
ViniciusK-PC <viniciuskb36@gmail.com>
2025-11-03 06:59:12 -0300

    fix: Correçoes

--- components/Frontend/Services/ServiceCard.tsx
+++ components/Frontend/Services/ServiceCard.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
+import { ServiceProps } from "@/types/type";
 
-export default function ServiceCard() {
+export default function ServiceCard({service}:
+    {service:ServiceProps}) {
     return (
         <Link 
-        href="#" className="rounded-md bg-slate-100
+        href={`/services/${service.slug}`}
+        className="rounded-md bg-slate-100
          hover:bg-slate-200 duration-300 flex gap-4 overflow-hidden">
 
             <Image 
-            src="/img1.jpg" 
+            src={service.image}
             width={1170} 
             height={848} 
-            alt="title" 
+            alt={service.title}
             className="w-1/3 object-cover aspect-video" 
             />
             
             <div className="flex flex-col w-2/3 py-4">
-                <h2>TeleSaúde</h2>
+                <h2>{service.title}</h2>
                 <p className="text-[0.6rem]">1 Pisicologa Avaliada</p>
             </div>
         </Link>
