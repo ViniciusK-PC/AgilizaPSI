e03dc6d322aeb025e4cd627fc00a2d6f25ec02dc
ViniciusK-PC <viniciuskb36@gmail.com>
2025-11-03 06:57:52 -0300

    feat: Add LinkCard

--- components/Frontend/Doctors/LinkCard.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link"
-
-export default function LinkCard({className}:{className:string}) {
-    return (
-        <Link href="#" className={`flex gap-4 bg`}>
-         <h2>Ansiedade</h2>
-        </Link>
-    )
-}
