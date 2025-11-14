3c1aaa9b9ff9b78f327d3248d44677e48febeda8
ViniciusK-PC <viniciuskb36@gmail.com>
2025-11-14 14:59:57 -0300

    fix Adiçao dashBoard

--- components/Frontend/Navbar.tsx
+++ components/Frontend/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
     <header className="fixed top-0 bg-white border-b border-gray-400/30 w-full z-50">
       <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2.5 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <Link href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Sua Compania</span>
             <img
               alt=""
